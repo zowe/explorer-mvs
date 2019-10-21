ca6e6212470d2025f47e35531e29564309d825ab
Nakul Manchanda <nakul.manchanda@ibm.com>
2019-10-21 09:23:37 -0400

    refactor from mvs-explorer to explorer-mvs
    Signed-off-by: Nakul Manchanda <nakul.manchanda@ibm.com>

--- scripts/explorer-mvs-start.sh
+++ scripts/explorer-mvs-start.sh
@@ -23,12 +23,12 @@
 . $ROOT_DIR/scripts/utils/validateNode.sh
 NODE_BIN=${NODE_HOME}/bin/node
 
-cd "$ROOT_DIR/components/mvs-explorer/bin"
+cd "$ROOT_DIR/components/explorer-mvs/bin"
 EXPLORER_PLUGIN_BASEURI=$($NODE_BIN -e "process.stdout.write(require('./app/package.json').config.baseuri)")
 EXPLORER_PLUGIN_NAME=$($NODE_BIN -e "process.stdout.write(require('./app/package.json').config.pluginName)")
 
 # get current ui server directory
-SERVER_DIR="${ROOT_DIR}/components/mvs-explorer/bin/server/"
+SERVER_DIR="${ROOT_DIR}/components/explorer-mvs/bin/server/"
 
 # start service
 $NODE_BIN $SERVER_DIR/src/index.js \
