1ce80b06d68690a154c879e174d3e6b319ec25c0
Jordan Cain <jordan.cain1@uk.ibm.com>
2020-06-03 15:48:22 -0400

    Test sequential dataset context menus
    Signed-off-by: Jordan Cain <jordan.cain1@uk.ibm.com>

--- tests/FVTTests/DatasetInteration/testContextMenu.ts
+++ tests/FVTTests/DatasetInteration/testContextMenu.ts
@@ -10,25 +10,30 @@
 import { expect } from 'chai';
 import { WebDriver, By, WebElement, until } from "selenium-webdriver";
 import { getDriver, setApimlAuthTokenCookie, loadPage, testElementAppearsXTimesById } from "explorer-fvt-utilities";
-import { editDatasetQualifierField } from "../utilities";
+import { editDatasetQualifierField, createTestSequentialDataset, createTestPartitionedDataset, cleanupDatasets } from "../utilities";
 import {
     USERNAME,
     PASSWORD,
     BASE_URL,
     BASE_URL_WITH_PATH,
+    TEST_PARTITIONED_DATASET,
+    TEST_SEQUENTIAL_DATASET,
 } from '../environment';
 
-describe.only('Test Context Menu for datasets', function () {
+describe('Test Context Menu for datasets', function () {
     let driver: WebDriver;
     this.retries(3);
     
     before('Initialise', async () => {
         driver = await getDriver();
         await setApimlAuthTokenCookie(driver, USERNAME, PASSWORD, `${BASE_URL}/api/v1/gateway/auth/login`, BASE_URL_WITH_PATH);
+        await createTestPartitionedDataset();
+        await createTestSequentialDataset();
     });
 
     after('Close out', async () => {
         driver.quit();
+        await cleanupDatasets();
     });
 
     describe('Context menu loads on right click', () => {
@@ -37,42 +42,17 @@ describe.only('Test Context Menu for datasets', function () {
             await driver.wait(until.elementLocated(By.id('refresh-icon')));
         });
 
-        async function openContextMenu() {
-            await editDatasetQualifierField(driver, 'USER.**');
+        async function openContextMenu(dataset) {
+            await editDatasetQualifierField(driver, `${dataset}`);
             const datasets: WebElement[] = await driver.findElements(By.className('node-label'));
 
             await driver.actions().contextClick(datasets[0]).perform();
         }
 
-        it('Should display a context menu on right click on dataset' , async () => {
-            await openContextMenu();
-
-            const contextMenu :WebElement[] = await driver.findElements(By.css('.react-contextmenu--visible'));
-            expect(contextMenu).to.be.an('array').with.length(1);
-        });
-
-        it('Should display context menu with context menu items', async () => {
-            await openContextMenu();
-
-            const contextMenuItems :WebElement[] = await driver.findElements(By.css('nav.react-contextmenu--visible > div.react-contextmenu-item'));
-            expect(contextMenuItems).to.be.an('array').with.length.greaterThan(0);
-        });
-
         async function getContextMenuItemByIndex(index :number) {
             const contextMenuItems :WebElement[] = await driver.findElements(By.css('nav.react-contextmenu--visible > div.react-contextmenu-item'));
             return contextMenuItems[index];
         }
-
-        it('Should display context menu with expected menu items on Partitioned Dataset', async () => {
-            const expectedContextMenuItems = ['New Dataset...', 'New Dataset Member...', 'Delete', 'Rename'];
-            await openContextMenu();
-            for (let i = 0; i <= 3; i++) {
-                const contextMenuItem :WebElement = await getContextMenuItemByIndex(i);
-                await driver.sleep(200);
-                const menuItemText :string = await contextMenuItem.getText();
-                expect(menuItemText).to.equal(expectedContextMenuItems[i]);
-            }
-        });
 
         async function testCorrectDialogLoads(expectedDialogTitle :string) {
             expect(await testElementAppearsXTimesById(driver, 'dialog', 1)).to.be.true;
@@ -87,32 +67,107 @@ describe.only('Test Context Menu for datasets', function () {
             await contextMenuItem.click();
         }
 
-        it('Should render create new dataset dialog when clicking New Dataset...', async () => {
-            expect(await testElementAppearsXTimesById(driver, 'dialog', 0)).to.be.true;
-            await openContextMenu();
-            await openDialogByContextMenuId(0);
-            await testCorrectDialogLoads('New Dataset');
+        describe('Paritioned Datasets', () => {
+            it('Should display a context menu on right click' , async () => {
+                await openContextMenu(TEST_PARTITIONED_DATASET);
+
+                const contextMenu :WebElement[] = await driver.findElements(By.css('.react-contextmenu--visible'));
+                expect(contextMenu).to.be.an('array').with.length(1);
+            });
+
+            it('Should display context menu with context menu items', async () => {
+                await openContextMenu(TEST_PARTITIONED_DATASET);
+
+                const contextMenuItems :WebElement[] = await driver.findElements(By.css('nav.react-contextmenu--visible > div.react-contextmenu-item'));
+                expect(contextMenuItems).to.be.an('array').with.length.greaterThan(0);
+            });
+
+            it('Should display context menu with expected menu items', async () => {
+                const expectedContextMenuItems = ['New Dataset...', 'New Dataset Member...', 'Delete', 'Rename'];
+                await openContextMenu(TEST_PARTITIONED_DATASET);
+                for (let i = 0; i <= 3; i++) {
+                    const contextMenuItem :WebElement = await getContextMenuItemByIndex(i);
+                    await driver.sleep(200);
+                    const menuItemText :string = await contextMenuItem.getText();
+                    expect(menuItemText).to.equal(expectedContextMenuItems[i]);
+                }
+            });
+
+            it('Should render create new dataset dialog when clicking New Dataset...', async () => {
+                expect(await testElementAppearsXTimesById(driver, 'dialog', 0)).to.be.true;
+                await openContextMenu(TEST_PARTITIONED_DATASET);
+                await openDialogByContextMenuId(0);
+                await testCorrectDialogLoads('New Dataset');
+            });
+
+            it('Should render new dataset member dialog when clicking New Dataset Member...', async () => {
+                expect(await testElementAppearsXTimesById(driver, 'dialog', 0)).to.be.true;
+                await openContextMenu(TEST_PARTITIONED_DATASET);
+                await openDialogByContextMenuId(1);
+                await testCorrectDialogLoads('New Dataset Member');
+            });
+
+            it('Should render delete dataset member dialog when clicking Delete', async () => {
+                expect(await testElementAppearsXTimesById(driver, 'dialog', 0)).to.be.true;
+                await openContextMenu(TEST_PARTITIONED_DATASET);
+                await openDialogByContextMenuId(2);
+                await testCorrectDialogLoads('Delete');
+            });
+
+            it('Should render rename dataset member dialog when clicking Rename', async () => {
+                expect(await testElementAppearsXTimesById(driver, 'dialog', 0)).to.be.true;
+                await openContextMenu(TEST_PARTITIONED_DATASET);
+                await openDialogByContextMenuId(3);
+                await testCorrectDialogLoads('Rename');
+            });
         });
 
-        it('Should render new dataset member dialog when clicking New Dataset Member...', async () => {
-            expect(await testElementAppearsXTimesById(driver, 'dialog', 0)).to.be.true;
-            await openContextMenu();
-            await openDialogByContextMenuId(1);
-            await testCorrectDialogLoads('New Dataset Member');
-        });
+        describe('Sequential Datasets', () => {
+            it('Should display a context menu on right click' , async () => {
+                await openContextMenu(TEST_SEQUENTIAL_DATASET);
 
-        it('Should render new dataset member dialog when clicking Delete', async () => {
-            expect(await testElementAppearsXTimesById(driver, 'dialog', 0)).to.be.true;
-            await openContextMenu();
-            await openDialogByContextMenuId(2);
-            await testCorrectDialogLoads('Delete');
-        });
+                const contextMenu :WebElement[] = await driver.findElements(By.css('.react-contextmenu--visible'));
+                expect(contextMenu).to.be.an('array').with.length(1);
+            });
 
-        it('Should render new dataset member dialog when clicking Rename', async () => {
-            expect(await testElementAppearsXTimesById(driver, 'dialog', 0)).to.be.true;
-            await openContextMenu();
-            await openDialogByContextMenuId(3);
-            await testCorrectDialogLoads('Rename');
-        });
+            it('Should display context menu with context menu items', async () => {
+                await openContextMenu(TEST_SEQUENTIAL_DATASET);
+
+                const contextMenuItems :WebElement[] = await driver.findElements(By.css('nav.react-contextmenu--visible > div.react-contextmenu-item'));
+                expect(contextMenuItems).to.be.an('array').with.length.greaterThan(0);
+            });
+
+            it('Should display context menu with expected menu items', async () => {
+                const expectedContextMenuItems = ['New Dataset...', 'Open', 'Delete', 'Submit as Job', 'Rename'];
+                await openContextMenu(TEST_SEQUENTIAL_DATASET);
+                for (let i = 0; i <= 3; i++) {
+                    const contextMenuItem :WebElement = await getContextMenuItemByIndex(i);
+                    await driver.sleep(200);
+                    const menuItemText :string = await contextMenuItem.getText();
+                    expect(menuItemText).to.equal(expectedContextMenuItems[i]);
+                }
+            });
+
+            it('Should render create new dataset dialog when clicking New Dataset...', async () => {
+                expect(await testElementAppearsXTimesById(driver, 'dialog', 0)).to.be.true;
+                await openContextMenu(TEST_SEQUENTIAL_DATASET);
+                await openDialogByContextMenuId(0);
+                await testCorrectDialogLoads('New Dataset');
+            });
+
+            it('Should render delete dataset member dialog when clicking Delete', async () => {
+                expect(await testElementAppearsXTimesById(driver, 'dialog', 0)).to.be.true;
+                await openContextMenu(TEST_SEQUENTIAL_DATASET);
+                await openDialogByContextMenuId(2);
+                await testCorrectDialogLoads('Delete');
+            });
+
+            it('Should render rename dataset member dialog when clicking Rename', async () => {
+                expect(await testElementAppearsXTimesById(driver, 'dialog', 0)).to.be.true;
+                await openContextMenu(TEST_SEQUENTIAL_DATASET);
+                await openDialogByContextMenuId(4);
+                await testCorrectDialogLoads('Rename');
+            });
+        })
     });
 });
