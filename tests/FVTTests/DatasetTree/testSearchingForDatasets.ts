f1db678b1a63df72b2463bd85f529d3d50b434c2
Adarshdeep Cheema <adarshdeep.cheema@ibm.com>
2021-12-09 16:20:08 -0500

    investigating the refresh Icon transition testcase intermediate failure
    Signed-off-by: Adarshdeep Cheema <adarshdeep.cheema@ibm.com>

--- tests/FVTTests/DatasetTree/testSearchingForDatasets.ts
+++ tests/FVTTests/DatasetTree/testSearchingForDatasets.ts
@@ -35,53 +35,42 @@ describe('Test searching for datasets', function () {
             await driver.wait(until.elementLocated(By.id('refresh-icon')));
         });
 
-        // it('Should have editable qualifier field', async () => {
-        //     const qualifierField: WebElement = await driver.findElement(By.id("datasets-qualifier-field"));
-        //     const initialText: string = await qualifierField.getAttribute('value');
-        //     const newText: string = 'ABC123!#';
-        //     await qualifierField.sendKeys(newText);
-        //     const qualifierFieldModified: WebElement = await driver.findElement(By.id("datasets-qualifier-field"));
-        //     const modifiedText: string = await qualifierFieldModified.getAttribute('value');
-        //     expect(modifiedText).to.equal(initialText + newText);
-        // });
-
-        // it('Should return datasets matching new qualifier', async () => {
-        //     expect(await testQualifierSearch(driver, 'USER.**', 'USER')).to.be.true;;
-        // });
-
-        // it('Should return datasets matching new multiple levels of qualifiers', async () => {
-        //     expect(await testQualifierSearch(driver, 'USER.PARMLIB.**', 'USER.PARMLIB')).to.be.true;;
-        // });
-
-        // it('Should return no datasets found message when using crazy qualifier', async () => {
-        //     expect(await testQualifierSearch(driver, 'ABCZYX12', 'No Datasets found')).to.be.true;;
-        // });
-
-        // async function testQualifierSearch(driver: WebDriver, searchQualifier: string, matchQualifier: string) {
-        //     await editDatasetQualifierField(driver, searchQualifier);
-
-        //     const datasets: WebElement[] = await driver.findElements(By.className('node-label'));
-        //     let allQualifiersMatch: boolean = true;
-        //     datasets.forEach(async (nodeLabel: WebElement) => {
-        //         const nodeLabelText: string = await nodeLabel.getText();
-        //         if (!nodeLabelText.includes(matchQualifier)) {
-        //             console.log('Unexpected dataset label: ' + nodeLabelText + ' should have included: ' + matchQualifier);
-        //             allQualifiersMatch = false;
-        //         }
-        //     });
-        //     return allQualifiersMatch;
-        // }
-
-        it('Should change refresh icon to loading and then back to refresh when clicking refresh icon', async () => {
-            try {
-                const refreshIcon: WebElement = await driver.findElement(By.id('refresh-icon'));
-            await refreshIcon.click();
-            }
-            catch(err) {
-                console.log('element not found or clicked ');
-            }
-            expect(await testRefreshIconTransition(driver)).to.be.true;
+        it('Should have editable qualifier field', async () => {
+            const qualifierField: WebElement = await driver.findElement(By.id("datasets-qualifier-field"));
+            const initialText: string = await qualifierField.getAttribute('value');
+            const newText: string = 'ABC123!#';
+            await qualifierField.sendKeys(newText);
+            const qualifierFieldModified: WebElement = await driver.findElement(By.id("datasets-qualifier-field"));
+            const modifiedText: string = await qualifierFieldModified.getAttribute('value');
+            expect(modifiedText).to.equal(initialText + newText);
         });
+
+        it('Should return datasets matching new qualifier', async () => {
+            expect(await testQualifierSearch(driver, 'USER.**', 'USER')).to.be.true;;
+        });
+
+        it('Should return datasets matching new multiple levels of qualifiers', async () => {
+            expect(await testQualifierSearch(driver, 'USER.PARMLIB.**', 'USER.PARMLIB')).to.be.true;;
+        });
+
+        it('Should return no datasets found message when using crazy qualifier', async () => {
+            expect(await testQualifierSearch(driver, 'ABCZYX12', 'No Datasets found')).to.be.true;;
+        });
+
+        async function testQualifierSearch(driver: WebDriver, searchQualifier: string, matchQualifier: string) {
+            await editDatasetQualifierField(driver, searchQualifier);
+
+            const datasets: WebElement[] = await driver.findElements(By.className('node-label'));
+            let allQualifiersMatch: boolean = true;
+            datasets.forEach(async (nodeLabel: WebElement) => {
+                const nodeLabelText: string = await nodeLabel.getText();
+                if (!nodeLabelText.includes(matchQualifier)) {
+                    console.log('Unexpected dataset label: ' + nodeLabelText + ' should have included: ' + matchQualifier);
+                    allQualifiersMatch = false;
+                }
+            });
+            return allQualifiersMatch;
+        }
 
         it('Should show loading icon after changing qualifier field then go back to refresh icon', async () => {
             const qualifierField: WebElement = await driver.findElement(By.id('datasets-qualifier-field'));
@@ -89,16 +78,16 @@ describe('Test searching for datasets', function () {
             expect(await testRefreshIconTransition(driver)).to.be.true;
         });
 
+        it('Should change refresh icon to loading and then back to refresh when clicking refresh icon', async () => {
+            driver.findElement(By.id('refresh-icon')).click();
+            expect(await testRefreshIconTransition(driver)).to.be.true;
+        });
+
         async function testRefreshIconTransition(driver) {
             try {
-                const loadingIcon: WebElement = await driver.findElement(By.id('loading-icon'));
-                console.log('trying to find the element and it is found ');
-
                 await driver.wait(until.elementLocated(By.id('loading-icon')), 10000);
-
-
             } catch(err) {
-                console.log('[testRefreshIconTransition] fail on loading-icon not found' + err);
+                console.log('[testRefreshIconTransition] fail on loading-icon not found');
                 return false;
             }
             await driver.wait(until.elementLocated(By.id('refresh-icon')), 30000);
