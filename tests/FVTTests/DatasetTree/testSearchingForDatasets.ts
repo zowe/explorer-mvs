a53715dbd34b8b3dd68c80edd95f05cfd887b41d
Adarshdeep Cheema <adarshdeep.cheema@ibm.com>
2021-12-09 14:07:28 -0500

    reverting back the last commit
    Signed-off-by: Adarshdeep Cheema <adarshdeep.cheema@ibm.com>

--- tests/FVTTests/DatasetTree/testSearchingForDatasets.ts
+++ tests/FVTTests/DatasetTree/testSearchingForDatasets.ts
@@ -81,7 +81,6 @@ describe('Test searching for datasets', function () {
         it('Should change refresh icon to loading and then back to refresh when clicking refresh icon', async () => {
             const refreshIcon: WebElement = await driver.findElement(By.id('refresh-icon'));
             await refreshIcon.click();
-            await driver.sleep(500); //Wait for dataset members to start fetching
             expect(await testRefreshIconTransition(driver)).to.be.true;
         });
 
