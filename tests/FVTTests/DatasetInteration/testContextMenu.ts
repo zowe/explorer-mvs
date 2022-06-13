17e9b68d28265b9aa4f470e776f3e952df9760de
Adarshdeep Cheema <adarshdeep.cheema@ibm.com>
2022-06-13 11:11:09 -0400

    reverting the last commit
    Signed-off-by: Adarshdeep Cheema <adarshdeep.cheema@ibm.com>

--- tests/FVTTests/DatasetInteration/testContextMenu.ts
+++ tests/FVTTests/DatasetInteration/testContextMenu.ts
@@ -175,7 +175,7 @@ describe('Test Context Menu for datasets', function () {
 
             it('Should display contect menu with expected items', async () => {
                 await openDatasetMemberContextMenu(TEST_PARTITIONED_DATASET, TEST_DATASET_MEMBER);
-                const expectedContextMenuItems = ['New Dataset Member...', 'Open', 'Delete Member', 'Submit as Job', 'Rename', 'Download'];
+                const expectedContextMenuItems = ['New Dataset Member...', 'Open', 'Delete Member', 'Submit as Job', 'Rename'];
                 const actualContextMenuItems = await getContextMenuItems();
                 expect(expectedContextMenuItems).to.eql(actualContextMenuItems);                
             });
@@ -219,7 +219,7 @@ describe('Test Context Menu for datasets', function () {
 
             it('Should display context menu with expected menu items', async () => {
                 await findDatasetAndOpenContextMenu(TEST_SEQUENTIAL_DATASET);
-                const expectedContextMenuItems = ['New Dataset...', 'Open', 'Delete', 'Submit as Job', 'Rename', 'Download'];
+                const expectedContextMenuItems = ['New Dataset...', 'Open', 'Delete', 'Submit as Job', 'Rename'];
                 const actualContextMenuItems = await getContextMenuItems();
                 expect(expectedContextMenuItems).to.eql(actualContextMenuItems);
             });
