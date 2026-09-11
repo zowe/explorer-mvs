d771bd0bd72d0f2afec365f67738bf900cc4a00c
Pavel Zlatnik <pavel.zlatnik@broadcom.com>
2026-09-11 10:22:02 +0200

    remove test server from utils
    Signed-off-by: Pavel Zlatnik <pavel.zlatnik@broadcom.com>

--- WebContent/js/utilities/urlUtils.ts
+++ WebContent/js/utilities/urlUtils.ts
@@ -14,10 +14,7 @@ export function encodeURLComponent(URL: string) {
 }
 
 export function whichServer() {
-    let server = global.location.host;
-    if (global.location.hostname === 'tester.test.com') {
-        server = 'tester.test.com:7443';
-    }
+    let server = global.location.host;    
     return server;
 }
 
